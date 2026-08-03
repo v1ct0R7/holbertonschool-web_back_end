18128ff8b10147dedfd14a2e784ac345eb52bc78
Elvis Murati <shtepiaime200@gmail.com>
2026-08-04 00:33:31 +0200

    complete task-2

--- ES6_basic/2-main.js
+++ ES6_basic/2-main.js
@@ -2,3 +2,4 @@ import getNeighborhoodsList from "./2-arrow";
 
 const neighborhoodsList = new getNeighborhoodsList();
 const res = neighborhoodsList.addNeighborhood("Noe Valley");
+console.log(res);
