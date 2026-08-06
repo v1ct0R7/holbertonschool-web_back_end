4ecf208f0b3418521f4fb0f422e2174afa1ce848
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-06 16:06:50 +0200

    make some changes on the task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
   for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+    array[i] = appendString + idx;
+    i += 1;    
   }
-
   return array;
 }
