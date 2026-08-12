b911a015aa7853918557550ebb62c39bdb185ff2
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-13 00:07:56 +0200

    complete task_6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,5 +4,17 @@ import uploadPhoto from "./5-photo-reject"
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
-  return Promise.allSettled([userPromise, photoPromise]);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+        return results.map((result) => {
+            if (result.status === "fulfilled") {
+                return result
+            } else {
+                return {
+                    status: result.status,
+                    value: result.reason,
+                };
+            }
+        })
+    })
 }
