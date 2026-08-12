1644ac6ffd7f7b8342d34cd1580343d476a471db
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-12 23:28:57 +0200

    complete task_6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from "./4-user-promise"
+import uploadPhoto from "./5-photo-reject"
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([userPromise, photoPromise]);
+}
