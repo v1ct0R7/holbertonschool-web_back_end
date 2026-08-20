1d1e568dd7eb89f0a03d92307e116b24e8282e8b
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-20 21:21:00 +0200

    correct some code in exercise 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,19 +2,14 @@ import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
-    results.map((result) => {
-      if (result.status === "fulfilled") {
-        return result;
-      }
-
-      return {
-        status: result.status,
-        value: result.reason.message,
-      };
-    }),
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value:
+        result.status === "fulfilled" ? result.value : result.reason.message,
+    })),
   );
 }
