6e077658fd377b92ef834a370ca306df5a802bbf
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-24 11:08:52 +0200

    complete task_1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 import ClassRoom from "./0-classroom"
 
-function initializeRooms() {
+export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
