cf13b60a55d9e5e6e5f1f0cc1c4ec19fe36647ad
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-24 17:07:56 +0200

    correct  task_6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -9,6 +9,6 @@ export default class SkyHighBuilding extends Building{
       return this._floors;
     }
     evacuationWarningMessage() {
-      return "Evacuate slowly the"+ " " + this.floors + " " + "floors.";
+      return "Evacuate slowly the"+ " " + this.floors + " " + "floors";
     }
 }
