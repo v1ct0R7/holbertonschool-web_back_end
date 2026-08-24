4851ec23c96d46d14f0418f2135b7adf25c99e1a
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-24 16:28:05 +0200

    complete task 5

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -15,8 +15,6 @@ module.exports = {
   },
   plugins: ["jest"],
   rules: {
-    "max-classes-per-file": "off",
-    "no-underscore-dangle": "off",
     "no-console": "off",
     "no-shadow": "off",
     "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
