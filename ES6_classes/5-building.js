e695aa2267471604ff0dea3499278c0f18b227a4
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-24 16:39:03 +0200

    correct task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    if (this.constructor === Building || !this.evacuationWarningMessage) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw new Error(
         "Class extending Building must override evacuationWarningMessage",
       );
