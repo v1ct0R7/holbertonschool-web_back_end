6a1050e488573c9b9f5a656d96e7e92687e5c091
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-24 15:44:47 +0200

    complete task_4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,24 @@
+export default class Pricing{
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency
+    }
+    get amount() {
+        return this._amount;
+    }
+    get currency() {
+        return this._currency;
+    }
+    set amount(value) {
+        this._amount = value;
+    }
+    set currency(value) {
+        this._currency = value;
+    }
+    displayFullPrice() {
+        return (this._amount + " " + this.currency.name + "(" + this.currency.code + ")")
+    }
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
