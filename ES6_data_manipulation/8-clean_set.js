44816cc36083ac3bc3e0442c17ab768e274ac4f5
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-26 12:01:43 +0200

    correct task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== "string" || startString.length === 0) {
+    return "";
+  }
+
   const values = [...set];
 
   return values
