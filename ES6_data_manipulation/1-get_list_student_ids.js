9c959d747c9123fae0909012a11fcba817a940b8
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-08-26 11:46:39 +0200

    complete task 1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(getListStudents) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+
+  return getListStudents.map((student) => student.id);
+}
