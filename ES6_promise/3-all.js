f459179dc8bd3eca42913afe977ae5340cf972b7
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-15 14:15:38 +0200

    Complete task

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,15 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from "./utils.js";
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(
-        `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`,
-      );
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log("Signup system offline");
     });
 }
-
-export default handleProfileSignup;
