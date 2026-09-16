786c9f02975345f95f28bdd51c4fe3255510b7f9
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-16 16:46:46 +0200

    Correct task 6

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,3 +1,4 @@
+-- 6-bonus.sql
 DELIMITER $$
 
 CREATE PROCEDURE AddBonus(
@@ -6,23 +7,25 @@ CREATE PROCEDURE AddBonus(
     IN score INT
 )
 BEGIN
-    DECLARE proj_id INT;
+    DECLARE project_id INT;
 
-    -- Kontrollo nëse projekti ekziston
-    SELECT id INTO proj_id
+    -- Check if the project already exists
+    SELECT id INTO project_id
     FROM projects
     WHERE name = project_name
     LIMIT 1;
 
-    -- Nëse s'ekziston, krijoje
-    IF proj_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET proj_id = LAST_INSERT_ID();
+    -- If it doesn't exist, create it
+    IF project_id IS NULL THEN
+        INSERT INTO projects(name)
+        VALUES (project_name);
+
+        SET project_id = LAST_INSERT_ID();
     END IF;
 
-    -- Shto korrigjimin e ri
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, proj_id, score);
-END$$
+    -- Add the correction
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+END $$
 
 DELIMITER ;
