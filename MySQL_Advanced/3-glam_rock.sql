13e0beb440d869326882b1701d2962c43670c00e
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-16 15:50:25 +0200

    Complete task 4

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,5 @@
 -- lists all bands with Glam rock as their main style
-SELECT DISTINCT style FROM metal_bands WHERE style LIKE '%Glam%'
-ORDER BY lifespan DESC, band_name ASC;
+SELECT band_name, IFNULL(split, 2024) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- krijojm nje trigger
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
