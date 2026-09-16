23b03d2ce47b93c90b83ea3a5bd216f7ccb8a121
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-16 16:39:26 +0200

    Complete task 10

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- 10-div.sql
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+
+    RETURN a / b;
+END $$
+
+DELIMITER ;
