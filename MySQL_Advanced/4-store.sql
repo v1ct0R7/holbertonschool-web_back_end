9b57a16b4daf074fda94528e27be7708e14e699a
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-16 14:58:34 +0200

    Correct task 4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style
-SELECT style, INFULL(split, 2024) - formed AS lifespan
+SELECT band_name, IFNULL(split, 2024) - formed AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock' AS band_name
-OREDER BY lifespan DESC;
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
